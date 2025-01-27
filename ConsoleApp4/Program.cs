6e76412921fb2481221a4055bae46d026dcbf68f
vojte <vojte@PC-DOMA>
2025-01-27 14:07:18 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int hadanecislo = 0;
+        int nahodnecislo = random.Next(1, 101);
+
+        while (hadanecislo != nahodnecislo)
+        {
+            Console.Write("Hádejte číslo: ");
+
+            try
+            {
+                hadanecislo = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Zadejte platné celé číslo.");
+                continue;
+            }
+
+            if (hadanecislo < nahodnecislo)
+            {
+                Console.WriteLine("Hledané číslo je větší.");
+            }
+            else if (hadanecislo > nahodnecislo)
+            {
+                Console.WriteLine("Hledané číslo je menší.");
+            }
+        }
+
+        Console.WriteLine("Gratuluji, našel jsi číslo!");
+        Console.ReadKey();
+    }
+}
